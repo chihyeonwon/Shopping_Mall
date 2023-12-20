b132d1032d947aa49c1eff1194fca289fbb274ee
ylose <heungeob1003@gmail.com>
2023-12-20 11:41:53 +0900

    상품 엔티티 매핑

--- src/main/java/com/shop/entity/Item.java
+++ src/main/java/com/shop/entity/Item.java
@@ -5,23 +5,35 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+import jakarta.persistence.*;
 import java.time.LocalDateTime;
 
+@Entity
+@Table(name="item")
 @Getter
 @Setter
 @ToString
 public class Item {
 
+    @Id
+    @Column(name="item_id")
+    @GeneratedValue(strategy=GenerationType.AUTO)
     private Long id; // 상품 코드
 
+    @Column(nullable = false, length=50)
     private String itemNm; // 상품명
 
+    @Column(name="price", nullable = false)
     private int price; // 가격
 
-    private int stockNumber; // 재고수량
+    @Column(nullable = false)
+    private int stockNumber; // 재고 수량
 
+    @Lob
+    @Column(nullable = false)
     private String itemDetail; // 상품 상세 설명
 
+    @Enumerated(EnumType.STRING)
     private ItemSellStatus itemSellStatus; // 상품 판매 상태
 
     private LocalDateTime regTime; // 등록 시간
