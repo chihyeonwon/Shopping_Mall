63eba358c77a5e50807ec484024eba9222178c52
ylose <heungeob1003@gmail.com>
2023-12-21 20:18:51 +0900

    상품 조회 쿼리 메소드 테스트

---
+++ src/test/java/com/shop/repository/ItemRepositoryTest.java
@@ -0,0 +1,50 @@
+package com.shop.repository;
+
+import com.shop.constant.ItemSellStatus;
+import com.shop.entity.Item;
+import net.bytebuddy.asm.Advice;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@TestPropertySource(locations="classpath:application-test.properties")
+class ItemRepositoryTest {
+
+    @Autowired
+    ItemRepository itemRepository;
+
+    @Test
+    @DisplayName("상품 저장 테스트")
+    public void createItemTest() {
+        for (int i = 1; i <= 10; i++) {
+            Item item = new Item();
+            item.setItemNm("테스트 상품" + i);
+            item.setPrice(10000 + i);
+            item.setItemDetail("테스트 상품 상세 설명" + i);
+            item.setItemSellStatus(ItemSellStatus.SELL);
+            item.setStockNumber(100);
+            item.setRegTime(LocalDateTime.now());
+            item.setUpdateTime(LocalDateTime.now());
+
+            Item savedItem = itemRepository.save(item); // itemRepository 인터페이스를 사용하여 item엔티티 저장
+        }
+    }
+
+    @Test
+    @DisplayName("상품 조회 테스트")
+    public void findByItemNmTest() {
+        this.createItemTest();
+        List<Item> itemList = itemRepository.findByItemNm("테스트 상품1");
+        for(Item item: itemList) {
+            System.out.println(item.toString());
+        }
+    }
+}
