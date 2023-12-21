759646e6797bd40f416c58b6da5e999c79d1a741
ylose <heungeob1003@gmail.com>
2023-12-21 20:18:37 +0900

    상품 조회 쿼리 메소드 작성

---
+++ src/main/java/com/shop/repository/ItemRepository.java
@@ -0,0 +1,10 @@
+package com.shop.repository;
+
+import com.shop.entity.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ItemRepository extends JpaRepository<Item, Long> {
+    List<Item> findByItemNm(String itemNm);
+}
