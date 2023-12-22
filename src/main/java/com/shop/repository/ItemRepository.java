b7684a297dc6318e6ff66385e35daa937bf21216
ylose <heungeob1003@gmail.com>
2023-12-22 14:40:59 +0900

    상품가격을 기준으로 내림차순 OrderBy 메소드 작성

--- src/main/java/com/shop/repository/ItemRepository.java
+++ src/main/java/com/shop/repository/ItemRepository.java
@@ -11,4 +11,6 @@ public interface ItemRepository extends JpaRepository<Item, Long> {
     List<Item> findByItemNmOrItemDetail(String itemNm, String itemDetail);
 
     List<Item> findByPriceLessThan(Integer price);
+
+    List<Item> findByPriceLessThanOrderByPriceDesc(Integer price);
 }
