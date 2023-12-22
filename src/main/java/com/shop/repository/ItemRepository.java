a48713777b78fae6911e939f3cf683a1cc3e9ef4
ylose <heungeob1003@gmail.com>
2023-12-22 14:14:46 +0900

    상품가격을 기준으로 한 LessThan 조건 메소드 작성

--- src/main/java/com/shop/repository/ItemRepository.java
+++ src/main/java/com/shop/repository/ItemRepository.java
@@ -9,4 +9,6 @@ public interface ItemRepository extends JpaRepository<Item, Long> {
     List<Item> findByItemNm(String itemNm);
 
     List<Item> findByItemNmOrItemDetail(String itemNm, String itemDetail);
+
+    List<Item> findByPriceLessThan(Integer price);
 }
