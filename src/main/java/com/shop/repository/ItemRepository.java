2fe2dcda5381e8e9154a164ecadcb53a15fdc3b1
ylose <heungeob1003@gmail.com>
2023-12-22 20:56:34 +0900

    @Query 어노테이션을 이용한 검색 처리 메소드 작성

--- src/main/java/com/shop/repository/ItemRepository.java
+++ src/main/java/com/shop/repository/ItemRepository.java
@@ -5,6 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 public interface ItemRepository extends JpaRepository<Item, Long> {
     List<Item> findByItemNm(String itemNm);
 
@@ -13,4 +16,7 @@ public interface ItemRepository extends JpaRepository<Item, Long> {
     List<Item> findByPriceLessThan(Integer price);
 
     List<Item> findByPriceLessThanOrderByPriceDesc(Integer price);
+
+    @Query("select i from Item i where i.itemDetail like %:itemDetail% order by i.price desc")
+    List<Item> findByItemDetail(@Param("itemDetail") String itemDetail);
 }
