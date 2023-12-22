f3bdaf30fa9cc7129a358a4d501f14b857222721
ylose <heungeob1003@gmail.com>
2023-12-22 14:03:36 +0900

    상품명과 상품 상세 설명 OR 조건 메소드 작성

--- src/main/java/com/shop/repository/ItemRepository.java
+++ src/main/java/com/shop/repository/ItemRepository.java
@@ -7,4 +7,6 @@ import java.util.List;
 
 public interface ItemRepository extends JpaRepository<Item, Long> {
     List<Item> findByItemNm(String itemNm);
+
+    List<Item> findByItemNmOrItemDetail(String itemNm, String itemDetail);
 }
