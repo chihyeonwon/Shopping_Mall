a19a0ae16931bc3005a945d2d825b7d4b4facfa6
ylose <heungeob1003@gmail.com>
2023-12-22 21:02:35 +0900

    @Query -nativeQuery 속성을 사용한 상품 조회 메소드 작성

--- src/main/java/com/shop/repository/ItemRepository.java
+++ src/main/java/com/shop/repository/ItemRepository.java
@@ -19,4 +19,7 @@ public interface ItemRepository extends JpaRepository<Item, Long> {
 
     @Query("select i from Item i where i.itemDetail like %:itemDetail% order by i.price desc")
     List<Item> findByItemDetail(@Param("itemDetail") String itemDetail);
+
+    @Query(value="select * from item i where i.item_detail like %:itemDetail% order by i.price desc", nativeQuery=true)
+    List<Item> findByItemDetailByNative(@Param("itemDetail") String itemDetail);
 }
