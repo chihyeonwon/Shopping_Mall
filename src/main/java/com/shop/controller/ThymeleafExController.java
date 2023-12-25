03b2c538ad09a7af17047f6f4f09c389e58b1822
ylose <heungeob1003@gmail.com>
2023-12-25 16:44:15 +0900

    상품 데이터 출력용 컨트롤러 클래스 작성

--- src/main/java/com/shop/controller/ThymeleafExController.java
+++ src/main/java/com/shop/controller/ThymeleafExController.java
@@ -1,16 +1,31 @@
 package com.shop.controller;
 
+import com.shop.dto.ItemDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.time.LocalDateTime;
+
 @Controller
 @RequestMapping(value="/thymeleaf")
 public class ThymeleafExController {
-    @GetMapping(value="/ex01")
+    /*@GetMapping(value="/ex01")
     public String thymeleafExample01(Model model){
         model.addAttribute("data", "타임리프 예제입니다.");
         return "thymeleafEx/thymeleafEx01";
+    }*/
+
+    @GetMapping(value="/ex02")
+    public String thymeleafExample02(Model model){
+        ItemDto itemDto = new ItemDto();
+        itemDto.setItemDetail("상품 상세 설명");
+        itemDto.setItemNm("테스트 상품1");
+        itemDto.setPrice(10000);
+        itemDto.setRegTime(LocalDateTime.now());
+
+        model.addAttribute("itemDto", itemDto); // 생성한 itemDto 객체 전달
+        return "thymeleafEx/thymeleafEx02";
     }
 }
