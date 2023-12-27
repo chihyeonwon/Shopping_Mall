16717d67d998908cc2de55cbc56fd53ac5d81777
ylose <heungeob1003@gmail.com>
2023-12-27 22:30:40 +0900

    가입정보를 담을 MemberFormDto 클래스 작성

---
+++ src/main/java/com/shop/dto/MemberFormDto.java
@@ -0,0 +1,16 @@
+package com.shop.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MemberFormDto {
+    private String name;
+
+    private String email;
+
+    private String password;
+
+    private String address;
+}
