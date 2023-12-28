124a9479f661821087f5d106883d39642b8b5a23
ylose <heungeob1003@gmail.com>
2023-12-28 13:56:14 +0900

    회원 가입 컨트롤러 작성

--- src/main/java/com/shop/controller/MemberController.java
+++ src/main/java/com/shop/controller/MemberController.java
@@ -1,6 +1,5 @@
 package com.shop.controller;
 
-
 import com.shop.dto.MemberFormDto;
 import com.shop.service.MemberService;
 import lombok.RequiredArgsConstructor;
@@ -9,16 +8,32 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
+import com.shop.entity.Member;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+
 @RequestMapping("/members")
+@Controller
 @RequiredArgsConstructor
 public class MemberController {
 
     private final MemberService memberService;
+    private final PasswordEncoder passwordEncoder;
 
-    @GetMapping(value="/new")
+    @GetMapping(value = "/new")
     public String memberForm(Model model){
         model.addAttribute("memberFormDto", new MemberFormDto());
         return "member/memberForm";
     }
+
+    @PostMapping(value = "/new")
+    public String newMember(MemberFormDto memberFormDto){
+
+        Member member = Member.createMember(memberFormDto, passwordEncoder);
+        memberService.saveMember(member);
+
+        return "redirect:/";
+    }
+
+
 }
