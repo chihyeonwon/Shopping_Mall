60a95cf1e30a12f9b1f7c7f3a6899188b2f3e6e9
ylose <heungeob1003@gmail.com>
2023-12-28 12:18:19 +0900

    회원가입 기능 테스트 코드 작성

---
+++ src/test/java/com/shop/service/MemberServiceTest.java
@@ -0,0 +1,46 @@
+package com.shop.service;
+
+import com.shop.dto.MemberFormDto;
+import com.shop.entity.Member;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.TestPropertySource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+@TestPropertySource(locations="classpath:application-test.properties")
+class MemberServiceTest {
+
+    @Autowired
+    MemberService memberService;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    public Member createMember(){
+        MemberFormDto memberFormDto = new MemberFormDto();
+        memberFormDto.setEmail("test@email.com");
+        memberFormDto.setName("홍길동");
+        memberFormDto.setAddress("서울시 마포구 합정동");
+        memberFormDto.setPassword("1234");
+        return Member.createMember(memberFormDto, passwordEncoder);
+    }
+
+    @Test
+    @DisplayName("회원가입 테스트")
+    public void saveMemberTest(){
+        Member member = createMember();
+        Member savedMember = memberService.saveMember(member);
+
+        assertEquals(member.getEmail(), savedMember.getEmail());
+        assertEquals(member.getName(), savedMember.getName());
+        assertEquals(member.getAddress(), savedMember.getAddress());
+        assertEquals(member.getPassword(), savedMember.getPassword());
+        assertEquals(member.getRole(), savedMember.getRole());
+    }
+}
