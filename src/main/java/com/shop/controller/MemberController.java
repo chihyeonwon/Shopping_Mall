ba1ee2072e606e7558233a0aec1692bbe07bc58e
ylose <heungeob1003@gmail.com>
2023-12-29 13:34:58 +0900

    회원 정보 검증 및 중복회원 가입 예외 처리 추가

--- src/main/java/com/shop/controller/MemberController.java
+++ src/main/java/com/shop/controller/MemberController.java
@@ -2,9 +2,11 @@ package com.shop.controller;
 
 import com.shop.dto.MemberFormDto;
 import com.shop.service.MemberService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -27,10 +29,19 @@ public class MemberController {
     }
 
     @PostMapping(value = "/new")
-    public String newMember(MemberFormDto memberFormDto){
+    public String newMember(@Valid MemberFormDto memberFormDto, BindingResult bindingResult, Model model){
 
-        Member member = Member.createMember(memberFormDto, passwordEncoder);
-        memberService.saveMember(member);
+        if(bindingResult.hasErrors()){ // 에러가 있다면 다시 회원가입 페이지로 이동
+            return "member/memberForm";
+        }
+
+        try {
+            Member member = Member.createMember(memberFormDto, passwordEncoder);
+            memberService.saveMember(member);
+        } catch(IllegalStateException e) {
+            model.addAttribute("errorMessage", e.getMessage());
+            return "member/memberForm";
+        }
 
         return "redirect:/";
     }
