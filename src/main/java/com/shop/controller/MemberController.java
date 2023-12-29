8751b00e14556711ecd6e0518e958aeefa5cfea6
ylose <heungeob1003@gmail.com>
2023-12-29 14:54:24 +0900

    로그인 페이지와 연결되도록 컨트롤러 수정

--- src/main/java/com/shop/controller/MemberController.java
+++ src/main/java/com/shop/controller/MemberController.java
@@ -46,5 +46,15 @@ public class MemberController {
         return "redirect:/";
     }
 
+    @GetMapping(value="/login")
+    public String loginMember(){
+        return "/member/memberLoginForm";
+    }
+
+    @GetMapping(value="/login/error")
+    public String loginError(Model model){
+        model.addAttribute("loginErrorMsg", "아이디 또는 비밀번호를 확인해주세요");
+        return "/member/memberLoginForm";
+    }
 
 }
