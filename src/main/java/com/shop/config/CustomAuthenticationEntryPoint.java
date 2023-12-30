ed4b59a429fa71f56a6b3b8e3c31d2762c0344b7
ylose <heungeob1003@gmail.com>
2023-12-30 13:57:34 +0900

    인증되지 않은 사용자의 XMLHttpRequest 요청 처리

---
+++ src/main/java/com/shop/config/CustomAuthenticationEntryPoint.java
@@ -0,0 +1,21 @@
+package com.shop.config;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response,
+                         AuthenticationException authException) throws IOException, ServletException {
+        if ("XMLHttpRequest".equals(request.getHeader("x-requested-with"))) {
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
+        } else {
+            response.sendRedirect("/members/login");
+        }
+    }
+}
