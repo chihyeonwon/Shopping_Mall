fcbbb2cebecf8a9c01d482b70dbb43f9e1c025a0
ylose <heungeob1003@gmail.com>
2024-01-01 14:22:45 +0900

    주문 엔티티 설계

---
+++ src/main/java/com/shop/entity/Order.java
@@ -0,0 +1,35 @@
+package com.shop.entity;
+
+import com.shop.constant.OrderStatus;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter @Setter
+@Entity
+@Table(name="orders")
+public class Order {
+
+    @Id @GeneratedValue
+    @Column(name="order_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name="member_id")
+    private Member member;
+
+    private LocalDateTime orderDate; // 주문일
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus orderStatus; // 주문상태
+
+    @OneToMany(mappedBy="order")
+    private List<OrderItem> orderItems = new ArrayList<>();
+
+    private LocalDateTime regTime; // 등록시간
+
+    private LocalDateTime updateTime; // 수정시간
+}
