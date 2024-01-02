708f4a56b5147b80e093d7e11039b1c1fa0ca018
ylose <heungeob1003@gmail.com>
2024-01-02 19:03:46 +0900

    주문 엔티티를 저장하기 위한 OrderRepository 인터페이스 작성

---
+++ src/main/java/com/shop/repository/OrderRepository.java
@@ -0,0 +1,7 @@
+package com.shop.repository;
+
+import com.shop.entity.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+}
