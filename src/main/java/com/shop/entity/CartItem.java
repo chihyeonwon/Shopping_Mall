ea12bb24d58c0702b9209840ddd5130398619515
ylose <heungeob1003@gmail.com>
2024-01-03 12:41:34 +0900

    fetch 전략을 지연 로딩으로 설정

--- src/main/java/com/shop/entity/CartItem.java
+++ src/main/java/com/shop/entity/CartItem.java
@@ -13,11 +13,11 @@ public class CartItem {
     @Column(name="cart_item_id")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch=FetchType.LAZY)
     @JoinColumn(name="cart_id")
     private Cart cart;
 
-    @ManyToOne
+    @ManyToOne(fetch=FetchType.LAZY)
     @JoinColumn(name="item_id")
     private Item item;
 
