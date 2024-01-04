0824043d5beb87ec01c4d915215c4c6fa975ceb6
ylose <heungeob1003@gmail.com>
2024-01-04 18:36:41 +0900

    상품 이미지 데이터 전달용 DTO 생성

---
+++ src/main/java/com/shop/dto/ItemImgDto.java
@@ -0,0 +1,25 @@
+package com.shop.dto;
+
+import com.shop.entity.ItemImg;
+import lombok.*;
+import org.modelmapper.ModelMapper;
+
+@Getter @Setter
+public class ItemImgDto {
+
+    private Long id;
+
+    private String imgName;
+
+    private String oriImgName;
+
+    private String imgUrl;
+
+    private String repImgYn;
+
+    private static ModelMapper modelMapper = new ModelMapper();
+
+    public static ItemImgDto of(ItemImg itemImg){
+        return modelMapper.map(itemImg, ItemImgDto.class);
+    }
+}
