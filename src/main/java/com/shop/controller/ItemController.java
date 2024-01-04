b17635b51fe7b9006e3a8c1ecc9b2ba3ccdb8faa
ylose <heungeob1003@gmail.com>
2024-01-04 18:33:59 +0900

    상품 등록 페이지로 연결하는 컨트롤러 수정

--- src/main/java/com/shop/controller/ItemController.java
+++ src/main/java/com/shop/controller/ItemController.java
@@ -3,10 +3,15 @@ package com.shop.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import org.springframework.ui.Model;
+import com.shop.dto.ItemFormDto;
+
+
 @Controller
 public class ItemController {
-    @GetMapping(value="/admin/item/new")
-    public String itemForm(){
+    @GetMapping(value = "/admin/item/new")
+    public String itemForm(Model model){
+        model.addAttribute("itemFormDto", new ItemFormDto());
         return "item/itemForm";
     }
 }
