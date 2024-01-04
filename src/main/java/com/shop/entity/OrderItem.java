1eb47b6662225ff3fa3f38d53f4e474f86187fd6
ylose <heungeob1003@gmail.com>
2024-01-04 17:45:08 +0900

    BaseEntity를 상속하고 regTime, updateTime 변수 삭제

--- src/main/java/com/shop/entity/OrderItem.java
+++ src/main/java/com/shop/entity/OrderItem.java
@@ -8,7 +8,7 @@ import java.time.LocalDateTime;
 @Getter @Setter
 @Entity
 @Table(name="order_item")
-public class OrderItem {
+public class OrderItem extends BaseEntity {
 
     @Id @GeneratedValue
     @Column(name="order_item_id")
@@ -25,8 +25,4 @@ public class OrderItem {
     private int orderPrice; // 주문가격
 
     private int count; // 수량
-
-    private LocalDateTime regTime;
-
-    private LocalDateTime updateTime;
 }
