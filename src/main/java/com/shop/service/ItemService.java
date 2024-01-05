c3f153f1ee87aa7161c3d98d4c392b2f00a556cc
ylose <heungeob1003@gmail.com>
2024-01-05 13:26:29 +0900

    상품 이미지를 업로드하고, 이미지 정보를 저장하는 ItemImgService 클래스 작성

---
+++ src/main/java/com/shop/service/ItemService.java
@@ -0,0 +1,46 @@
+package com.shop.service;
+
+import com.shop.dto.ItemFormDto;
+import com.shop.entity.Item;
+import com.shop.entity.ItemImg;
+import com.shop.repository.ItemImgRepository;
+import com.shop.repository.ItemRepository;
+import com.shop.service.ItemImgService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@Service @Transactional @RequiredArgsConstructor
+public class ItemService {
+
+    private final ItemRepository itemRepository;
+
+    private final ItemImgService itemImgService;
+
+    private final ItemImgRepository itemImgRepository;
+
+    public Long saveItem(ItemFormDto itemFormDto, List<MultipartFile> itemImgFileList) throws Exception{
+
+        //상품 등록
+        Item item = itemFormDto.createItem();
+        itemRepository.save(item);
+
+        //이미지 등록
+        for(int i=0;i<itemImgFileList.size();i++){
+            ItemImg itemImg = new ItemImg();
+            itemImg.setItem(item);
+
+            if(i == 0)
+                itemImg.setRepimgYn("Y");
+            else
+                itemImg.setRepimgYn("N");
+
+            itemImgService.saveItemImg(itemImg, itemImgFileList.get(i));
+        }
+
+        return item.getId();
+    }
+}
