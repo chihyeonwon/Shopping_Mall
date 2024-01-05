2d4acb15a7093e46da6e172370e3380e2cdff4a8
ylose <heungeob1003@gmail.com>
2024-01-05 13:33:43 +0900

    상품의 이미지 정보를 저장하는 ItemImgRepository 인터페이스 작성

--- src/main/java/com/shop/repository/ItemImgRepository.java
+++ src/main/java/com/shop/repository/ItemImgRepository.java
@@ -5,7 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface ItemImgRepository extends JpaRepository<ItemImg, Long> {
-
     List<ItemImg> findByItemIdOrderByIdAsc(Long itemId);
 
 }
