97b7f5842104dc3745dca7f6ed3caf3124c1f862
ylose <heungeob1003@gmail.com>
2024-01-09 19:13:05 +0900

    getAdminItemPage 메소드를 정의한 사용자 정의 ItemRepositoryCustom 인터페이스 생성

---
+++ src/main/java/com/shop/repository/ItemRepositoryCustom.java
@@ -0,0 +1,11 @@
+package com.shop.repository;
+
+import com.shop.dto.ItemSearchDto;
+import com.shop.entity.Item;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface ItemRepositoryCustom {
+
+    Page<Item> getAdminItemPage(ItemSearchDto itemSearchDto, Pageable pageable);
+}
