4b32ebe7e3456aaf1daca6670d622126a61d120e
ylose <heungeob1003@gmail.com>
2024-01-10 20:17:21 +0900

    메인 페이지에 보여줄 상품 리스트를 가져오는 getMainItemPage 메소드 작성

--- src/main/java/com/shop/repository/ItemRepositoryCustom.java
+++ src/main/java/com/shop/repository/ItemRepositoryCustom.java
@@ -1,6 +1,7 @@
 package com.shop.repository;
 
 import com.shop.dto.ItemSearchDto;
+import com.shop.dto.MainItemDto;
 import com.shop.entity.Item;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -8,4 +9,7 @@ import org.springframework.data.domain.Pageable;
 public interface ItemRepositoryCustom {
 
     Page<Item> getAdminItemPage(ItemSearchDto itemSearchDto, Pageable pageable);
+
+    Page<MainItemDto> getMainItemPage(ItemSearchDto itemSearchDto, Pageable pageable);
+
 }
