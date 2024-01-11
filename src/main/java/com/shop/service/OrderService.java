740ed2376198a6560a0f5cc88d37b2d20aaebbf2
ylose <heungeob1003@gmail.com>
2024-01-11 17:28:01 +0900

    주문 로직을 작성하는 OrderService 작성

---
+++ src/main/java/com/shop/service/OrderService.java
@@ -0,0 +1,44 @@
+package com.shop.service;
+
+import com.shop.dto.OrderDto;
+import com.shop.entity.*;
+import com.shop.repository.ItemRepository;
+import com.shop.repository.MemberRepository;
+import com.shop.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import jakarta.persistence.EntityNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.shop.repository.ItemImgRepository;
+
+@Service @Transactional @RequiredArgsConstructor
+public class OrderService {
+
+    private final ItemRepository itemRepository;
+
+    private final MemberRepository memberRepository;
+
+    private final OrderRepository orderRepository;
+
+    private final ItemImgRepository itemImgRepository;
+
+    public Long order(OrderDto orderDto, String email){
+
+        Item item = itemRepository.findById(orderDto.getItemId())
+                .orElseThrow(EntityNotFoundException::new);
+
+        Member member = memberRepository.findByEmail(email);
+
+        List<OrderItem> orderItemList = new ArrayList<>();
+        OrderItem orderItem = OrderItem.createOrderItem(item, orderDto.getCount());
+        orderItemList.add(orderItem);
+        Order order = Order.createOrder(member, orderItemList);
+        orderRepository.save(order);
+
+        return order.getId();
+    }
+}
