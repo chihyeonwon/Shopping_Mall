8fbb601d6c9294d35b841000248718649cd63f1d
ylose <heungeob1003@gmail.com>
2024-01-11 17:26:51 +0900

    주문할 상품과 수량을 통해 OrderItem 객체를 만드는 createOrderItem 메소드 작성

--- src/main/java/com/shop/entity/OrderItem.java
+++ src/main/java/com/shop/entity/OrderItem.java
@@ -25,4 +25,18 @@ public class OrderItem extends BaseEntity {
     private int orderPrice; // 주문가격
 
     private int count; // 수량
+
+    public static OrderItem createOrderItem(Item item, int count){
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setCount(count);
+        orderItem.setOrderPrice(item.getPrice());
+
+        item.removeStock(count);
+        return orderItem;
+    }
+
+    public int getTotalPrice(){
+        return orderPrice * count;
+    }
 }
