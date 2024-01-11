63e63ce22a2ebe56e43d01ca2e156c807df20d95
ylose <heungeob1003@gmail.com>
2024-01-11 17:27:38 +0900

    주문할 상품의 아이디와 주문 수량을 전달받은 OrderDto 클래스 작성

---
+++ src/main/java/com/shop/dto/OrderDto.java
@@ -0,0 +1,18 @@
+package com.shop.dto;
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+@Getter @Setter
+public class OrderDto {
+
+    @NotNull(message = "상품 아이디는 필수 입력 값입니다.")
+    private Long itemId;
+
+    @Min(value = 1, message = "최소 주문 수량은 1개입니다.")
+    @Max(value = 999, message = "최대 주문 수량은 999개입니다.")
+    private int count;
+
+}
