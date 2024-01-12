7cb2d72d225fc7394e97532fb7caca78416a40ad
ylose <heungeob1003@gmail.com>
2024-01-12 17:49:10 +0900

    상품의 대표 이미지를 찾는 쿼리 메소드를 추가

--- src/main/java/com/shop/repository/ItemImgRepository.java
+++ src/main/java/com/shop/repository/ItemImgRepository.java
@@ -7,4 +7,6 @@ import java.util.List;
 public interface ItemImgRepository extends JpaRepository<ItemImg, Long> {
     List<ItemImg> findByItemIdOrderByIdAsc(Long itemId);
 
+    ItemImg findByItemIdAndRepimgYn(Long itemId, String repimgYn);
+
 }
