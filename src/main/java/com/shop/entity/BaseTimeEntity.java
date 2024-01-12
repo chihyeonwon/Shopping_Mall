e462fbefd15c2a6def5cec1c7e1548719fb0ec12
ylose <heungeob1003@gmail.com>
2024-01-12 17:19:27 +0900

    import jakarta.persistence.*

--- src/main/java/com/shop/entity/BaseTimeEntity.java
+++ src/main/java/com/shop/entity/BaseTimeEntity.java
@@ -1,14 +1,12 @@
 package com.shop.entity;
 
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.EntityListeners;
-import jakarta.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 
 @EntityListeners(value = {AuditingEntityListener.class})
@@ -22,5 +20,4 @@ public abstract class BaseTimeEntity {
 
     @LastModifiedDate
     private LocalDateTime updateTime;
-
 }
