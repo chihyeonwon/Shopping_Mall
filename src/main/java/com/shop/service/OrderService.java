c5aca613ad26d160c4394129da2a4bdd982808fe
ylose <heungeob1003@gmail.com>
2024-01-14 02:57:16 +0900

    주문을 취소하는 cancelOrder 메서드, 주문자와 현재 사용자를 검증하는 validateOrder 메서드 추가

--- src/main/java/com/shop/service/OrderService.java
+++ src/main/java/com/shop/service/OrderService.java
@@ -19,6 +19,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.shop.repository.ItemImgRepository;
+import org.thymeleaf.util.StringUtils;
 
 @Service @Transactional @RequiredArgsConstructor
 public class OrderService {
@@ -70,5 +71,25 @@ public class OrderService {
         }
 
         return new PageImpl<OrderHistDto>(orderHistDtos, pageable, totalCount);
+    }
+
+    @Transactional(readOnly = true)
+    public boolean validateOrder(Long orderId, String email){
+        Member curMember = memberRepository.findByEmail(email);
+        Order order = orderRepository.findById(orderId)
+                .orElseThrow(EntityNotFoundException::new);
+        Member savedMember = order.getMember();
+
+        if(!StringUtils.equals(curMember.getEmail(), savedMember.getEmail())){
+            return false;
+        }
+
+        return true;
+    }
+
+    public void cancelOrder(Long orderId){
+        Order order = orderRepository.findById(orderId)
+                .orElseThrow(EntityNotFoundException::new);
+        order.cancelOrder();
     }
 }
