0b6cb33c0a0b67e118830c986c2d2512bc5e0dc1
ylose <heungeob1003@gmail.com>
2024-01-14 02:58:07 +0900

    주문 취소 로직을 호출하는 cancelOrder 메서드 추가

--- src/main/java/com/shop/controller/OrderController.java
+++ src/main/java/com/shop/controller/OrderController.java
@@ -65,4 +65,14 @@ public class OrderController {
         return "order/orderHist";
     }
 
+    @PostMapping("/order/{orderId}/cancel")
+    public @ResponseBody ResponseEntity cancelOrder(@PathVariable("orderId") Long orderId , Principal principal){
+
+        if(!orderService.validateOrder(orderId, principal.getName())){
+            return new ResponseEntity<String>("주문 취소 권한이 없습니다.", HttpStatus.FORBIDDEN);
+        }
+
+        orderService.cancelOrder(orderId);
+        return new ResponseEntity<Long>(orderId, HttpStatus.OK);
+    }
 }
