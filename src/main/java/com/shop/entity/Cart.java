89f01224cb4ab095830f52bf529bfc44a232d192
ylose <heungeob1003@gmail.com>
2024-01-16 14:22:48 +0900

    장바구니 엔티티를 생성하는 createCart 메소드 작성

--- src/main/java/com/shop/entity/Cart.java
+++ src/main/java/com/shop/entity/Cart.java
@@ -17,4 +17,10 @@ public class Cart extends BaseEntity{
     @OneToOne(fetch=FetchType.LAZY)
     @JoinColumn(name="member_id")
     private Member member;
+
+    public static Cart createCart(Member member){
+        Cart cart = new Cart();
+        cart.setMember(member);
+        return cart;
+    }
 }
