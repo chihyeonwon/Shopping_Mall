2aa7db2c967eb0ae7523e1063269f1f1aa67952d
ylose <heungeob1003@gmail.com>
2024-01-18 01:40:05 +0900

    현재 장바구니에 담겨있는 수량을 변경하는 updateCount 메소드 추가

--- src/main/java/com/shop/entity/CartItem.java
+++ src/main/java/com/shop/entity/CartItem.java
@@ -34,4 +34,8 @@ public class CartItem extends BaseEntity {
     public void addCount(int count){
         this.count += count;
     }
+
+    public void updateCount(int count){
+        this.count = count;
+    }
 }
