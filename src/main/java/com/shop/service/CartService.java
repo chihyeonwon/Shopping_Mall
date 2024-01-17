ef983a58f7b4a3f784c50029d2df53516e6dd87c
ylose <heungeob1003@gmail.com>
2024-01-17 14:00:42 +0900

    현재 로그인한 회원의 정보를 이용하여 장바구니에 들어있는 상품을 조회하는 getCartList 메서드 작성

--- src/main/java/com/shop/service/CartService.java
+++ src/main/java/com/shop/service/CartService.java
@@ -1,5 +1,6 @@
 package com.shop.service;
 
+import com.shop.dto.CartDetailDto;
 import com.shop.dto.CartItemDto;
 import com.shop.entity.Cart;
 import com.shop.entity.CartItem;
@@ -13,6 +14,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import jakarta.persistence.EntityNotFoundException;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service @RequiredArgsConstructor @Transactional
 public class CartService {
@@ -45,4 +49,20 @@ public class CartService {
             return cartItem.getId();
         }
     }
+
+    @Transactional(readOnly = true)
+    public List<CartDetailDto> getCartList(String email){
+
+        List<CartDetailDto> cartDetailDtoList = new ArrayList<>();
+
+        Member member = memberRepository.findByEmail(email);
+        Cart cart = cartRepository.findByMemberId(member.getId());
+        if(cart == null){
+            return cartDetailDtoList;
+        }
+
+        cartDetailDtoList = cartItemRepository.findCartDetailDtoList(cart.getId());
+        return cartDetailDtoList;
+    }
+
 }
