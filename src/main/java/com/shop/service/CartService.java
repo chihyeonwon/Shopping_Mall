16c297c67b1c64ca9b90400913e9e4866af5922d
ylose <heungeob1003@gmail.com>
2024-01-18 17:58:49 +0900

    장바구니 상품 번호를 파라미터로 받아서 삭제하는 deleteCartItem 메소드 작성

--- src/main/java/com/shop/service/CartService.java
+++ src/main/java/com/shop/service/CartService.java
@@ -85,4 +85,10 @@ public class CartService {
 
         cartItem.updateCount(count);
     }
+
+    public void deleteCartItem(Long cartItemId) {
+        CartItem cartItem = cartItemRepository.findById(cartItemId)
+                .orElseThrow(EntityNotFoundException::new);
+        cartItemRepository.delete(cartItem);
+    }
 }
