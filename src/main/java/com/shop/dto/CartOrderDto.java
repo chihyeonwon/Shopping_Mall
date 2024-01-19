ecf5f2ce970ef47aff625bad2d6efc063d39b38e
ylose <heungeob1003@gmail.com>
2024-01-19 17:36:34 +0900

    주문할 상품 데이터를 전달할 DTO 클래스 작성

---
+++ src/main/java/com/shop/dto/CartOrderDto.java
@@ -0,0 +1,15 @@
+package com.shop.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter @Setter
+public class CartOrderDto {
+
+    private Long cartItemId;
+
+    private List<CartOrderDto> cartOrderDtoList;
+
+}
